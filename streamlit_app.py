f43c4b4c3ae839c8fef32f707b382a7246d8f032
sujith kumar <148097955+sujith52@users.noreply.github.com>
2024-10-15 21:41:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ pip install --upgrade pip
 import streamlit as st
 
 st.write('Hellow world')
+st.title("The World Where It Is Begining!!")
+st.write("Hellow to my dear beginers who was always intrested in the world of novel reading. The question here was it would be a graet fate taht we peoplemare lokking forward to carry it")
